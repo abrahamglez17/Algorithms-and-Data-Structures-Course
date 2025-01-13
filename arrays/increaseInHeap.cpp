d96b006354bf63b840550fb9d115823b6bf5e3fd
Abraham González <65511679+abrahamglez17@users.noreply.github.com>
2025-01-13 15:54:56 -0600

    increase array in heap

---
+++ arrays/increaseInHeap.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *p, *q;
+
+    int i;
+
+    p = (int *)malloc(5 * sizeof(int));
+    p[0] = 3; p[1] = 5; p[2] = 7; p[3] = 9; p[4] = 11;
+
+    q = (int *)malloc(10 * sizeof(int));
+
+    for(i = 0; i < 5; i++)
+    {
+        printf("p[%d] = %d\n", i, p[i]);
+    }
+
+    // free(p);
+    // p = q;
+    // q = NULL;
+
+    for(i = 0; i < 10; i++)
+    {
+        printf("q[%d] = %d\n", i, q[i]);
+    }
+
+    return 0;
+}
