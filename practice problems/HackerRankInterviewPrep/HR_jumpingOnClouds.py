bb43397deede5944fe6b625998fefef21929f675
Abraham González <65511679+abrahamglez17@users.noreply.github.com>
2025-03-13 13:52:52 -0600

    solved hourglassSum and jumpingOnClouds

---
+++ practice problems/HackerRankInterviewPrep/HR_jumpingOnClouds.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'jumpingOnClouds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY c as parameter.
+#
+
+def jumpingOnClouds(c):
+    steps=0
+    i=0
+    while i<len(c)-1: #we dont need to jump the last step
+        if i+2<len(c) and c[i+2]==0:
+            i+=2
+        else:
+            i+=1
+        steps +=1    
+    return steps
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
