b9e9f48a946e52ed0bc5e7a86039cd55177b83a4
Abraham González <65511679+abrahamglez17@users.noreply.github.com>
2025-03-13 19:56:57 -0600

    solved 70

--- practice problems/Leetcode/70_climbStairs.py
+++ practice problems/Leetcode/70_climbStairs.py
@@ -1,3 +1,20 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        dp = [0]*(n+1) 
+        dp[0]=1 # reacheable in 1 way
+        dp[1]=1 # reacheable in 1 way
+        dp[2]=2 # reacheable in 2 ways
         
+        # n ways to reach the nth step
+        for i in range(3, n+1):
+            dp[i]=dp[i-1] + dp[i-2] 
+        return dp[n]
+    
+case1=2
+case2=15
+s= Solution()
+
+print(case1)
+print(s.climbStairs(case1))
+print(case2)
+print(s.climbStairs(case2))
